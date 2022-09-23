907706e3ee6fd95957d7c53352700d3e6d615897
Eduardo Ocampo <eocampo3@gmail.com>
2022-09-22 18:29:32 -0700

    Fixing spaces

--- Classical_Orbital_Elements/elliptic_orbit_long_acsend_node.py
+++ Classical_Orbital_Elements/elliptic_orbit_long_acsend_node.py
@@ -6,24 +6,25 @@ from scipy.spatial.transform import Rotation as R
 
 def plot():
 
-    # Inclination Defintion
+    # Orientation Defintion
     inclination_angle = 25 # degrees
-    r = R.from_euler('yz', [inclination_angle,60], degrees=True)
+    long_ascend_node_angle = 60 # degrees
+    r = R.from_euler('yz', [inclination_angle,long_ascend_node_angle], degrees=True)
     rotation = r.as_matrix()
 
     # Set Equatorial Plane shape
     plane_size = 7
     points = np.array([[ plane_size,    plane_size, 0],
-                        [-plane_size-5,  plane_size, 0],
-                        [ plane_size,   -plane_size, 0],
-                        [-plane_size-5, -plane_size, 0]])
+                       [-plane_size-5,  plane_size, 0],
+                       [ plane_size,   -plane_size, 0],
+                       [-plane_size-5, -plane_size, 0]])
 
     # Establish Central Body Reference Axis
     axes_scale = 2
     node_scale = 4.0 # For figure use only
     axes = np.array([[1,0,0],
-                    [0,1,0],
-                    [0,0,1]])
+                     [0,1,0],
+                     [0,0,1]])
     axes = axes*axes_scale
 
     # Spacecraft Trajectory
@@ -70,74 +71,74 @@ def plot():
 
     # Draw Orbit and Orbit Centers
     orbit = go.Scatter3d(x=pos[:,0], 
-                        y=pos[:,1],
-                        z=pos[:,2], mode='lines', name="Reference Orbit",
-                        line=dict(width=4,color='grey',dash='dashdot'),hoverinfo='text'
+                         y=pos[:,1],
+                         z=pos[:,2], mode='lines', name="Reference Orbit",
+                         line=dict(width=4,color='grey',dash='dashdot'),hoverinfo='text'
     )
     orbit_rot = go.Scatter3d(x=pos_rot[:,0], 
-                            y=pos_rot[:,1],
-                            z=pos_rot[:,2], mode='lines', 
-                            name="Inclination = {} degrees & LAN = {} degrees".format(inclination_angle,lan),
-                            line=dict(width=4,color='red')
+                             y=pos_rot[:,1],
+                             z=pos_rot[:,2], mode='lines', 
+                             name="Inclination = {} degrees & Ω = {} degrees".format(inclination_angle,lan),
+                             line=dict(width=4,color='red')
     )
 
     # Add Central Body as Point Mass
     center = go.Scatter3d(x=[0,0], 
-                        y=[0,0],
-                        z=[0,0], mode='markers', name = "Centeral Body",
-                        marker=dict(size=12,color='black'),showlegend=False
+                          y=[0,0],
+                          z=[0,0], mode='markers', name = "Centeral Body",
+                          marker=dict(size=12,color='black'),showlegend=False
     )
 
     # Draw Angular Momentum and Arc from z to h
     angular_momentum_vector = go.Scatter3d(x=[0,angular_momentum[0]],
-                                        y=[0,angular_momentum[1]],
-                                        z=[0,angular_momentum[2]],
-                                        mode="lines+text",
-                                        line=dict(width=5,color='orange'),
-                                        showlegend=False,text=["", "h"],
-                                        textposition="top center",hoverinfo='text'
+                                           y=[0,angular_momentum[1]],
+                                           z=[0,angular_momentum[2]],
+                                           mode="lines+text",
+                                           line=dict(width=5,color='orange'),
+                                           showlegend=False,text=["", "h"],
+                                           textposition="top center",hoverinfo='text'
     )
     # Hack to get arrows:
     # https://stackoverflow.com/questions/66789390/draw-an-arrow-between-two-specific-points-in-a-scatter-plot-with-plotly-graph-ob
     arrow_tip_ratio = 0.4
     arrow_starting_ratio = 0.99
     angular_momentum_arrow = go.Cone(x=[0 + arrow_starting_ratio*(angular_momentum[0] - 0)],
-                                    y=[0 + arrow_starting_ratio*(angular_momentum[1] - 0)],
-                                    z=[0 + arrow_starting_ratio*(angular_momentum[2] - 0)],
-                                    u=[arrow_tip_ratio*(angular_momentum[0] - 0)],
-                                    v=[arrow_tip_ratio*(angular_momentum[1] - 0)],
-                                    w=[arrow_tip_ratio*(angular_momentum[2] - 0)],
-                                    showlegend=False,
-                                    showscale=False,
-                                    colorscale=[[0, 'orange'], [1, 'orange']]
+                                     y=[0 + arrow_starting_ratio*(angular_momentum[1] - 0)],
+                                     z=[0 + arrow_starting_ratio*(angular_momentum[2] - 0)],
+                                     u=[arrow_tip_ratio*(angular_momentum[0] - 0)],
+                                     v=[arrow_tip_ratio*(angular_momentum[1] - 0)],
+                                     w=[arrow_tip_ratio*(angular_momentum[2] - 0)],
+                                     showlegend=False,
+                                     showscale=False,
+                                     colorscale=[[0, 'orange'], [1, 'orange']]
     )
     draw_incl_arc = go.Scatter3d(x=arc[:,0], 
-                            y=arc[:,1],
-                            z=arc[:,2], 
-                            mode='lines+text', 
-                            name="e = {}".format(e),
-                            text=[""]*int(len(arc_z)/2)+["i"],
-                            textposition="top center",
-                            line=dict(width=4,color='orange'),
-                            hoverinfo='text',showlegend=False
+                                 y=arc[:,1],
+                                 z=arc[:,2], 
+                                 mode='lines+text', 
+                                 name="e = {}".format(e),
+                                 text=[""]*int(len(arc_z)/2)+["i"],
+                                 textposition="top center",
+                                 line=dict(width=4,color='orange'),
+                                 hoverinfo='text',showlegend=False
     )
     draw_lan_arc = go.Scatter3d(x=arc_lan[:,0], 
-                            y=arc_lan[:,1],
-                            z=arc_lan[:,2], 
-                            mode='lines+text', 
-                            name="e = {}".format(e),
-                            text=[""]*int(len(arc_z)*3/4)+["LAN"],
-                            textposition="middle center",
-                            line=dict(width=4,color='green'),
-                            hoverinfo='text',showlegend=False
+                                y=arc_lan[:,1],
+                                z=arc_lan[:,2], 
+                                mode='lines+text', 
+                                name="e = {}".format(e),
+                                text=[""]*int(len(arc_z)*3/4)+["Ω"],
+                                textposition="middle center",
+                                line=dict(width=4,color='green'),
+                                hoverinfo='text',showlegend=False
     )
 
     # Draw Reference Planes
     equator_plane = go.Mesh3d(x=points[:,0],y=points[:,1],z=points[:,2],
-                            opacity=0.5,
-                            color='lightblue',
-                            text="Equatorial Plane",
-                            hoverinfo="text")
+                              opacity=0.5,
+                              color='lightblue',
+                              text="Equatorial Plane",
+                              hoverinfo="text")
 
     lower_plane = go.Mesh3d(x=points[:,0],y=points[:,1],z=points[:,2]-5,
                             opacity=0.5,
@@ -146,22 +147,22 @@ def plot():
 
     # Draw Reference Axes
     axes_x = go.Scatter3d(x=[0,axes[0,0]],y=[0,axes[0,1]],z=[0,axes[0,2]],
-                        mode="lines+text",
-                        line=dict(width=4,color='grey'),
-                        showlegend=False,text=["", "X Axis"],
-                        textposition="bottom center",hoverinfo='text'
+                          mode="lines+text",
+                          line=dict(width=4,color='grey'),
+                          showlegend=False,text=["", "X Axis"],
+                          textposition="bottom center",hoverinfo='text'
     )
     axes_y = go.Scatter3d(x=[0,axes[1,0]],y=[0,axes[1,1]],z=[0,axes[1,2]],
-                        mode="lines+text",
-                        line=dict(width=4,color='grey'),
-                        showlegend=False,text=["", "Y Axis"],
-                        textposition="bottom center",hoverinfo='text'
+                          mode="lines+text",
+                          line=dict(width=4,color='grey'),
+                          showlegend=False,text=["", "Y Axis"],
+                          textposition="bottom center",hoverinfo='text'
     )
     axes_z = go.Scatter3d(x=[0,axes[2,0]],y=[0,axes[2,1]],z=[0,axes[2,2]],
-                        mode="lines+text",
-                        line=dict(width=4,color='black'),
-                        showlegend=False,text=["", "Z Axis"],
-                        textposition="bottom right",hoverinfo='text'
+                          mode="lines+text",
+                          line=dict(width=4,color='black'),
+                          showlegend=False,text=["", "Z Axis"],
+                          textposition="bottom right",hoverinfo='text'
     )
 
     # Draw Node Vector
@@ -176,14 +177,14 @@ def plot():
     node_tip_ratio = 0.1
     node_starting_ratio = 0.99
     node_arrow = go.Cone(x=[0 + node_starting_ratio*(node_vec[0]*node_scale - 0)],
-                        y=[0 + node_starting_ratio*(node_vec[1]*node_scale - 0)],
-                        z=[0 + node_starting_ratio*(node_vec[2]*node_scale - 0)],
-                        u=[node_tip_ratio*(node_vec[0]*node_scale - 0)],
-                        v=[node_tip_ratio*(node_vec[1]*node_scale - 0)],
-                        w=[node_tip_ratio*(node_vec[2]*node_scale - 0)],
-                        showlegend=False,
-                        showscale=False,
-                        colorscale=[[0, 'green'], [1, 'green']]
+                         y=[0 + node_starting_ratio*(node_vec[1]*node_scale - 0)],
+                         z=[0 + node_starting_ratio*(node_vec[2]*node_scale - 0)],
+                         u=[node_tip_ratio*(node_vec[0]*node_scale - 0)],
+                         v=[node_tip_ratio*(node_vec[1]*node_scale - 0)],
+                         w=[node_tip_ratio*(node_vec[2]*node_scale - 0)],
+                         showlegend=False,
+                         showscale=False,
+                         colorscale=[[0, 'green'], [1, 'green']]
     )
 
     # Define Figure and Add Drawings
@@ -208,16 +209,16 @@ def plot():
 
     # Update Figure
     fig.update_layout(scene = dict(camera = camera,
-                                xaxis = dict(nticks=4, range=[-plane_size-3,plane_size-3],
-                                                showgrid=False,visible=False,zeroline=False),
-                                yaxis = dict(nticks=4, range=[-plane_size,plane_size],
-                                                showgrid=False,visible=False,zeroline=False),
-                                zaxis = dict(nticks=4, range=[-plane_size,plane_size],
-                                                showgrid=False,zeroline=False,visible=False),
-                                aspectmode='cube'),
+                                   xaxis = dict(nticks=4, range=[-plane_size-3,plane_size-3],
+                                                   showgrid=False,visible=False,zeroline=False),
+                                   yaxis = dict(nticks=4, range=[-plane_size,plane_size],
+                                                   showgrid=False,visible=False,zeroline=False),
+                                   zaxis = dict(nticks=4, range=[-plane_size,plane_size],
+                                                   showgrid=False,zeroline=False,visible=False),
+                                   aspectmode='cube'),
                     margin=dict(r=10, l=10, b=1, t=10),
                     legend=dict(orientation="h",yanchor="bottom"),
-                    title={'text': "<b>Elliptical Orbit: Longitude of The Ascending Node (LAN) Change<b>",
+                    title={'text': "<b>Elliptical Orbit: Longitude of The Ascending Node (Ω) Change<b>",
                             'y':0.95,
                             'x':0.5,
                             'xanchor': 'center',
